e87b54f13443fd04f1f5eb48a91b41adf42d54a6
FiliTol <FiliTol@laptop.local>
2024-04-05 11:40:42 +0200

    Added custom config of CodeSnap to neovim lua config file

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,3 +7,14 @@ require("config.lazy")
 -- set shorter name for keymap function
 local kmap = vim.keymap.set
 require("lazyvim").setup({ colorscheme = "slate" })
+require("codesnap").setup({
+  mac_window_bar = false,
+  title = "",
+  code_font_family = "CaskaydiaCove Nerd Font",
+  watermark_font_family = "Pacifico",
+  watermark = "",
+  bg_theme = "default",
+  breadcrumbs_separator = "/",
+  has_breadcrumbs = true,
+  save_path = "/home/filippo/Pictures/CodeSnap/code.png",
+})
