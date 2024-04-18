b3571e415498c38a1c77e24da3de675280da3518
FiliTol <FiliTol@laptop.local>
2024-04-18 18:49:38 +0200

    Added quarto to files that can be previewed by markdown-preview.nvim

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -1,11 +1,14 @@
 return {
-
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = function()
       vim.fn["mkdp#util#install"]()
+    end,
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown", "quarto" }
+      vim.g.mkdp_browser = "/usr/bin/qutebrowser"
     end,
   },
 }
